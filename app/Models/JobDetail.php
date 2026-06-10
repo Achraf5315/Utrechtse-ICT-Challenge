6bfc4714b79ebbd9c4fd79b1a2d737dee705d4c9
Achraf <338127@student.mboutrecht.nl>
2026-06-10 14:51:10 +0200

    model ingevuld van JobDetail

--- app/Models/JobDetail.php
+++ app/Models/JobDetail.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobDetail extends Model
 {
-    //
+    protected $fillable = [
+        'description',
+        'requirements',
+        'responsibilities',
+        'benefits',
+    ];
+
+    public function job()
+    {
+        return $this->hasOne(Job::class);
+    }
 }
